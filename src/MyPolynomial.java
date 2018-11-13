f7e18b86526b32528b9e7ac6af6b1bb1725ceb34
alu20918149v <alu20918149v@ti2-01.informatica.lan>
2018-11-13 19:58:40 +0100

    Hello there!

--- src/MyPolynomial.java
+++ src/MyPolynomial.java
@@ -31,4 +31,17 @@ public class MyPolynomial {
 		return coeffs[coeffs.length-1];
 		
 	}
+	public MyPolynomial mult(MyPolynomial p) {
+		double[] result = new double[getDegree() + p.getDegree()];
+		for (int i=0; i<result.length; i++) {
+			result[i] = 0;
+		}
+		double[] coeffsP = p.getCoeffs();
+		for (int i=0; i<coeffs.length; i++) {
+			for (int j=0; j<coeffsP.length; j++) {
+				result[i + j] += coeffs[i] * coeffsP[j];
+			}
+		}
+		MyPolynomial newPol = new MyPolynomial(result);
+	}
 }
